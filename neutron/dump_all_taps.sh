61e2e7aaaf93e97e65ccd559f5235ead032d8847
int32bit <krystism@gmail.com>
2019-06-03 19:47:45 +0800

    Fix tap_to_port script

--- neutron/dump_all_taps.sh
+++ neutron/dump_all_taps.sh
@@ -2,7 +2,7 @@
 # Description: Dumps all tap devices to Neutron port.
 
 # Dump ovs tap devices
-for tap in $(ovs-vsctl show | awk '/Port "tap/{print $2}' | tr -d '"'); do
+for tap in $(ovs-vsctl show | grep -P 'Port "' | grep -Pv "(eth)|(qvo)" | tr -d '"' | awk '{print $2}'); do
     ./tap_to_port.sh $tap
 done
 
--- neutron/tap_to_port.sh
+++ neutron/tap_to_port.sh
@@ -13,10 +13,11 @@ PORT_PREFIX=${TAP_NAME:3}
 PORT_ID=$(neutron port-list | grep $PORT_PREFIX | cut -d '|' -f 2 | tr -d ' ')
 PORT_INFO_PATH=/tmp/port_info_${PORT_ID}
 neutron port-show $PORT_ID >$PORT_INFO_PATH
-IP=$(cat $PORT_INFO_PATH  | grep 'fixed_ips' | grep -Po '(\d{1,3}\.){3}\d{1,3}')
-MAC=$(cat $PORT_INFO_PATH | grep 'mac_address' | cut -d '|' -f 3 | tr -d ' ')
+IP=$(cat $PORT_INFO_PATH | awk -F '|' '/fixed_ips/{print $3}' | tr -d '"' | grep -Po "ip_address: [a-f0-9:.]+" | cut -d ':' -f 2- | tr -d ' ')
+MAC=$(cat $PORT_INFO_PATH | awk -F '|' '$2~/mac_address/{print $3}' | tr -d ' ')
 DEVICE_OWNER=$(cat $PORT_INFO_PATH | grep 'device_owner' | cut -d '|' -f 3 | tr -d ' ')
-printf "%s: %s %s %s\n" $PORT_ID $IP $MAC $DEVICE_OWNER
+
+printf "%s: %s  %s %s %s\n" $TAP_NAME $PORT_ID $IP $MAC $DEVICE_OWNER
 
 # Clean works
 rm -rf $PORT_INFO_PATH
